715258b3b8980d2cb3565f6cb048859d75f366e5
Miguel Guimar Salazar <116126244+Soymigueprogramador@users.noreply.github.com>
2024-08-16 16:27:09 -0300

    Agrege la dependencia de express.js y cree el servidor

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
-const nombreDelProyecto = 'MG-lo-quiero-3D';
-console.log(nombreDelProyecto);
+// Importaciones. ``
+import express from "express";
+
+// Constantes.
+const nameEcommerce = 'MG lo quiero 3D';
+const PORT = 8080;
+const app = express();
+
+// Users.
+app.use( express.urlencoded({ extended: true }));
+
+// Endpoint.
+app.get('/', ( req, res )=> {
+    res.send(`Bienvenidos a ${nameEcommerce}`); 
+});
+
+app.listen(PORT, ( req, res ) => {
+    console.log(` ${nameEcommerce} Esta funcionando en el puerto http://localhost:${PORT} `)
+});
